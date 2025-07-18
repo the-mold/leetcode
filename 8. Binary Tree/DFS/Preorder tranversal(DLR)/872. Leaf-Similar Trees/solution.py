a46a5978e2b1a0bf7f5a6e3a3edc47f0c56a4f6f
petromoldovan <petro.moldovan@gmail.com>
2025-07-18 07:55:17 +0200

    bt5

--- 8. Binary Tree/DFS/Preorder tranversal(DLR)/872. Leaf-Similar Trees/solution.py
+++ 8. Binary Tree/DFS/Preorder tranversal(DLR)/872. Leaf-Similar Trees/solution.py
@@ -6,22 +6,25 @@
 #         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        def helper(node, leaves):
+        def dfs(node, leaves):
             if not node:
-                return leaves
+                return
             
             if not node.left and not node.right:
                 leaves.append(node.val)
             
-            helper(node.left, leaves)
-            helper(node.right, leaves)
+            dfs(node.left, leaves)
+            dfs(node.right, leaves)
         
         leaves1 = []
+        dfs(root1, leaves1)
+        
         leaves2 = []
-        helper(root1, leaves1)
-        helper(root2, leaves2)
+        dfs(root2, leaves2)
 
         return leaves1 == leaves2
 
 #T:O(n)
 #S:O(n), worst case when there are completely skewed trees
+
+# Traverse: preorder
