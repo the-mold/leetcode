365ce1b5fd4050bf2b53d697bcd813bbdc3d2150
petromoldovan <petro.moldovan@gmail.com>
2025-09-02 18:09:28 +0200

    arrays22

---
+++ 2. Two Pointer/oposite_direction/977. Squares of a Sorted Array/bad-solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        res = []
+        for num in nums:
+            res.append(num**2)
+
+        return sorted(res)
+
+#T:O(n log(n))
+#S:O(n)
