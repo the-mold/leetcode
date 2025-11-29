df55e5a82acaf643a7acf2844f8f965567378f5f
petromoldovan <petro.moldovan@gmail.com>
2025-11-29 17:56:01 +0100

    dp13

---
+++ 13. Dynamic programming/nm/139. Word Break/bad-solution-with-string-manipulation.py
@@ -0,0 +1,20 @@
+# NOTE! Better use solution with index passing.
+def can_concat(s, words):
+  return _can_concat(s, words, {})
+
+def _can_concat(s, words, memo):
+  if s in memo:
+    return memo[s]
+    
+  if s == "":
+    return True
+
+  for word in words:
+    if s.startswith(word):
+      suffix = s[len(word):]
+      if _can_concat(suffix, words, memo):
+        memo[s] = True
+        return True
+
+  memo[s] = False
+  return False
