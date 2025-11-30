67b36535da6bf8830738dc944bbda6eb8340ac0f
Petro Moldovan <petro.moldovan@gmail.com>
2025-11-30 18:39:23 +0100

    strings3 (#142)

---
+++ 01. ArrayAndString/1400. Construct K Palindrome Strings/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        n = len(s)
+        # edge case: if more palidromes expected than there are charachters in string
+        if n < k:
+            return False
+    
+        count = [0] * 26
+        for ch in s:
+            count[ord(ch) - ord("a")] += 1
+
+        # Count how many characters have an odd frequency
+        odd_count = 0
+        for c in count:
+            if c % 2 != 0:
+                odd_count += 1
+
+        # 4. The number of odd-count characters must be less than or equal to k.
+        # Each odd-count character must be the center of a different palindrome.
+        return odd_count <= k
+      
+#T:O(n)
+#S:O(1)
